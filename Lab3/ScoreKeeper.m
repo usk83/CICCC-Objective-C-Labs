ec8d066a812da057b040a89e6633c279966c180f
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2020-03-25 23:30:08 -0700

    complete Lab3

---
+++ Lab3/ScoreKeeper.m
@@ -0,0 +1,13 @@
+#import "ScoreKeeper.h"
+
+@implementation ScoreKeeper
+
+- (NSString *)description {
+  return [NSString stringWithFormat:@"score: %ld right, %ld wrong ---- %.0f%%",
+    self.right,
+    self.wrong,
+    roundf((CGFloat)self.right * 100 / (self.right + self.wrong))
+  ];
+}
+
+@end
