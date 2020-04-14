602c0710962f6f5a6ba7383a93324a1115adf5b0
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2020-04-13 23:03:13 -0700

    implement roll command

--- Lab6/main.m
+++ Lab6/main.m
@@ -3,6 +3,16 @@
 
 static const int numberOfDice = 5;
 
+NSString *getUserInput() {
+  char inputChars[255];
+  char *result = fgets(inputChars, 255, stdin);
+  if (result == NULL) {
+    return NULL;
+  }
+  NSString *inputString = [[NSString stringWithUTF8String: inputChars] stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+  return inputString;
+}
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     NSLog(@"=== Threelow ===");
@@ -10,7 +20,16 @@ int main(int argc, const char * argv[]) {
     for (int i = numberOfDice; i > 0; i--) {
       [dice addObject: [Die new]];
     }
-    NSLog(@"%@", [dice componentsJoinedByString: @"  "]);
+    while (YES) {
+      NSLog(@"\n'roll' to roll the dice");
+      NSString *input = getUserInput();
+      if ([input isEqualToString: @"roll"]) {
+        for (Die *die in dice) {
+          [die roll];
+        }
+      }
+      NSLog(@"%@", [dice componentsJoinedByString: @"  "]);
+    }
   }
   return 0;
 }
